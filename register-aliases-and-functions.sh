ecbd417b9e688f1a5681fbfef46b9a68a7066480
Sebastian Peters <Sebastian.Peters@gmail.com>
2014-11-15 17:41:15 +0100

    fix bug in function dkrm remove unneeded semicolons add function dkipa for getting all container IDs

--- register-aliases-and-functions.sh
+++ register-aliases-and-functions.sh
@@ -1,23 +1,30 @@
 # inspired from http://www.kartar.net/2014/03/some-useful-docker-bash-functions-and-aliases/
 
-alias dki="docker images";
-alias dkps="docker ps -a";
+alias dki="docker images"
+alias dkps="docker ps -a"
 
 # stop all containers
-alias dkstopa="docker ps -q | xargs docker stop";
+alias dkstopa="docker ps -q | xargs docker stop"
 
 # remove all exited containers
 dkrme() {
-	docker ps -a | grep Exit | awk '{print $1}' | xargs docker rm;
-};
+	docker ps -a | grep Exit | awk '{print $1}' | xargs docker rm
+}
 
 # removes all non-running containers
-dkrm() { docker rm $(docker ps -q -a) };
+dkrm() {
+	docker rm $(docker ps -q -a)
+}
 
 # remove images labeled "<none>"
 dkrmi() {
-	docker rmi $(docker images | grep "^<none>" | awk '{print $3}');
-};
+	docker rmi $(docker images | grep "^<none>" | awk '{print $3}')
+}
 
-# Getting the IP address
-alias dkip="docker inspect --format '{{ .NetworkSettings.IPAddress }}'";
+# Getting the IP address of a given container
+alias dkip="docker inspect --format '{{ .NetworkSettings.IPAddress }}'"
+
+# get IP of every container
+dkipa() {
+	docker ps -q | xargs docker inspect | grep IPAddress | cut -d '"' -f 4
+}

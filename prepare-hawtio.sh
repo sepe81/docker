02b1fc61c43c1d1166d71b92c32bce2760c6648f
Sebastian Peters <Sebastian.Peters@gmail.com>
2015-02-01 14:39:39 +0100

    add script for download of latest hawtio

---
+++ prepare-hawtio.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+NAME=hawtio.war
+DOWNLOADS="$(dirname "$BASH_SOURCE")"/downloads
+WEBAPPS="$(dirname "$BASH_SOURCE")"/webapps
+
+if [ ! -f $DOWNLOADS/$NAME ]; then
+	mkdir -v -p $DOWNLOADS
+	curl -L -o $DOWNLOADS/$NAME "https://oss.sonatype.org/content/repositories/public/io/hawt/hawtio-default/1.4.46/hawtio-default-1.4.46.war";
+fi
+
+mkdir -v -p $WEBAPPS
+cp -v $DOWNLOADS/$NAME $WEBAPPS/$NAME 
